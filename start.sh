6ba68e559c2db0b126a12106f3ade5e1e0581022
dtcokr <53829474+dtcokr@users.noreply.github.com>
2021-03-02 22:49:31 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 chown -R ${PUID}:${PGID} /aria2
-exec su-exec "${PUID}:${PGID}" \
-  "/usr/bin/darkhttpd /aria2/webui --port 80 &" \
-exec su-exec "${PUID}:${PGID}" \
-  "/usr/bin/darkhttpd /downloads -p 81 &" \
-exec su-exec "${PUID}:${PGID}" \
-  "/usr/bin/aria2c --conf-path=/aria2/aria2.conf"
+exec su-exec ${PUID}:${PGID} \
+  /usr/bin/darkhttpd /aria2/webui --port 80 & \
+exec su-exec ${PUID}:${PGID} \
+  /usr/bin/darkhttpd /downloads -p 81 & \
+exec su-exec ${PUID}:${PGID} \
+  /usr/bin/aria2c --conf-path=/aria2/aria2.conf
